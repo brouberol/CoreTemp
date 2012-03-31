7f2cceab0b3109fb059e6e85326742a3043464d0
Balthazar Rouberol <rouberol.b@gmail.com>
2012-03-31 22:01:06 +0200

    New graph display Signed-off-by: Balthazar Rouberol <rouberol.b@gmail.com>

--- coretemp.py
+++ coretemp.py
@@ -11,7 +11,7 @@ from math import ceil
 NB_PROBES = 30
 
 def probe_temperature():
-    """ Returns the CPU and GPU temperature, in degrees centigrade (Celcius) """
+    """ Return the CPU and GPU temperature, in degrees centigrade (Celcius) """
     cmd = "inxi -s | grep Sensors | awk '{print $6,$10}'" # Get CPU/GPU temperatures 
     # INFO : inxi is a shell command sending back all sorts of informations: disk usage, cpu clock, sensors information, etc
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
@@ -53,22 +53,25 @@ def plot_temperatures(cpu_temps, gpu_temps):
     plt.grid()
     x = range(len(cpu_temps))
 
-    cpu_avg_plot = plt.axhline(y=sum([t for t in cpu_temps.values()])/float(len([t for t in cpu_temps.values()])), xmin=0, xmax=len(cpu_temps), color='r', linestyle='--')
-    gpu_avg_plot = plt.axhline(y=sum([t for t in gpu_temps.values()])/float(len([t for t in gpu_temps.values()])), xmin=0, xmax=len(gpu_temps), color='b', linestyle='--')
+    cpu_avg = sum([t for t in cpu_temps.values()])/float(len([t for t in cpu_temps.values()]))
+    gpu_avg = sum([t for t in gpu_temps.values()])/float(len([t for t in gpu_temps.values()]))
+    cpu_avg_plot = plt.axhline(y=cpu_avg, xmin=0, xmax=len(cpu_temps), color='r', linestyle='--')
+    gpu_avg_plot = plt.axhline(y=gpu_avg, xmin=0, xmax=len(gpu_temps), color='b', linestyle='--')
     cpu_plot = plt.plot(x, cpu_temps.values(), 'ro-')
     gpu_plot = plt.plot(x, gpu_temps.values(), 'bo-')
     locs, labels = plt.xticks(x, cpu_temps.keys())
-    plt.ylim(ymax = max([temp for temp in cpu_temps.values() + gpu_temps.values()]) + 1,
-             ymin = min([temp for temp in cpu_temps.values() + gpu_temps.values()]) - 1)
-    plt.xlim(xmax=len(x), xmin=-1)
+    plt.ylim(ymin = min([temp for temp in cpu_temps.values() + gpu_temps.values()]) - 1,
+             ymax = max([temp for temp in cpu_temps.values() + gpu_temps.values()]) + 3)
+    plt.xlim(xmin=-1, xmax=len(x))
     fig.autofmt_xdate() 
     plt.ylabel("Temperature (in Celcius)")
-    plt.legend([cpu_plot, gpu_plot], ["CPU", "GPU"])
-    plt.suptitle("Evolution of the CPU/GPU temperature", fontsize=25)
+    plt.legend([gpu_plot, gpu_avg_plot, cpu_plot, cpu_avg_plot], 
+               ["GPU temperature", "Average GPU temperature : %.1fC" %(gpu_avg), "CPU", "Average CPU temperature : %.1fC" %(cpu_avg)],
+               loc = "best")
+    plt.suptitle("Evolution of the CPU/GPU temperature\nbetween %s and %s" %(cpu_temps.keys()[0], cpu_temps.keys()[-1]), fontsize=20)
     plt.savefig("CPU_GPU_temperature.png")
 
 if __name__ == '__main__': 
     
-    cpu, gpu = probe_for_duration(hour=0, minute=30, second=0)
+    cpu, gpu = probe_for_duration(hour=0, minute=10, second=0)
     plot_temperatures(cpu, gpu)
-
